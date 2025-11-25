9501b64021e53ac12aa70ac3f8a3607249a38f8b
Oskar <proton2006tom@gmail.com>
2025-11-25 12:51:42 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,36 +9,50 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const TabList = ({ tabuls, selectedTab, setSelectedTab }) => {
+  const activeTab = tabuls.find(tab => tab.id === selectedTab);
 
+  return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
+          {tabuls.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === selectedTab ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <button
+                type="button"
+                className="tab-button"
+                data-cy="TabLink"
+                onClick={() => setSelectedTab(tab.id)}
+              >
+                {tab.title}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        Some text 1
+        {activeTab.content}
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+export const App = () => {
+  const [selectedTab, setSelectedTab] = useState('tab-1');
+
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {selectedTab}</h1>
+      <TabList
+        tabs={tabs}
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+      />
+    </div>
+  );
+};
